42a445be1e9a53a229ddb4e87db63cc5007cd524
TwentyXX <twenty.takumi1024@gmail.com>
2025-01-03 22:11:26 +0900

    refactor: Restructure key event handling with focused widget-specific logic

--- src/app/event.rs
+++ src/app/event.rs
@@ -26,27 +26,60 @@ impl App {
 	}
 
 	pub(crate) fn handle_key_event(&mut self, key_event: KeyEvent) {
-		match key_event.code {
-			KeyCode::Char('q') => self.exit(),
-			KeyCode::Left => self.decrement_counter(),
-			KeyCode::Right => self.increment_counter(),
-			KeyCode::Char(' ') => self.checkbox_state = !self.checkbox_state,
-			KeyCode::Tab => self.next_focus(),
-			KeyCode::Up => match self.focused_widget {
-				FocusedWidget::Slider => {
-					self.slider_value = self.slider_value.saturating_add(5).min(100)
-				}
-				_ => self.previous_focus(),
-			},
-			KeyCode::Down => match self.focused_widget {
-				FocusedWidget::Slider => self.slider_value = self.slider_value.saturating_sub(5),
-				_ => self.next_focus(),
-			},
-			KeyCode::Char(c) => self.input_text.push(c),
-			KeyCode::Backspace => {
-				self.input_text.pop();
+		let key_code = key_event.code;
+		match key_code {
+			KeyCode::Up => {
+				self.previous_focus();
+				return;
+			}
+			KeyCode::Down => {
+				self.next_focus();
+				return;
 			}
 			_ => {}
+		}
+		let a = (2, 3);
+		case!(a, {
+			(_, 2) => println!(),
+		});
+		
+		macro_rules! case {
+    ($value:expr, {$($pattern:pat => $result:expr),+}) => {
+        match $value {
+            $($pattern => {$result;return;},)*
+            _ => Default::default()
+        }
+    };
+}
+		match self.focused_widget {
+			FocusedWidget::Counter => match key_code {
+				KeyCode::Left => self.decrement_counter(),
+				KeyCode::Right => self.increment_counter(),
+				KeyCode::Char('q') => self.exit(),
+				KeyCode::Char(' ') => self.checkbox_state = !self.checkbox_state,
+				KeyCode::Tab => self.next_focus(),
+				KeyCode::Char(c) => self.input_text.push(c),
+				KeyCode::Backspace => {
+					self.input_text.pop();
+				}
+				_ => {}
+			},
+			FocusedWidget::Checkbox => match key_code {
+				KeyCode::Char(' ') => self.checkbox_state = !self.checkbox_state,
+				_ => {}
+			},
+			FocusedWidget::Slider => match key_code {
+				KeyCode::Left => self.slider_value = self.slider_value.saturating_sub(5),
+				KeyCode::Right => self.slider_value = self.slider_value.saturating_add(5).min(100),
+				_ => {}
+			},
+			FocusedWidget::Input => match key_code {
+				KeyCode::Char(c) => self.input_text.push(c),
+				KeyCode::Backspace => {
+					self.input_text.pop();
+				}
+				_ => {}
+			},
 		}
 	}
 }
