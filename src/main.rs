5e6835cc3a50fdc5392d2e1f318ac35f660d2677
TwentyXX <twenty.takumi1024@gmail.com>
2025-01-12 16:07:07 +0900

    added server listener

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod app;
+mod server;
 mod widget;
 
 use crate::app::App;
+use server::run_server;
 use std::io;
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
 	let mut terminal = ratatui::init();
 	let app_result = App::default().run(&mut terminal);
 	ratatui::restore();
+	run_server().await.unwrap();
 	app_result
 }
 
