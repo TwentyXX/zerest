3205676729b880e891352872adab5dc5aac22d0f
TwentyXX <twenty.takumi1024@gmail.com>
2025-02-17 22:51:17 +0900

    feat: include header information when POST

--- src/server/handlers/post.rs
+++ src/server/handlers/post.rs
@@ -1,18 +1,26 @@
-use chrono::Utc;
-
 use crate::{message::ServerMessage, server::ServerState};
-
-use axum::extract::Json;
-
-use axum::extract::State;
+use axum::{
+	extract::{Json, State},
+	http::HeaderMap,
+};
+use chrono::Utc;
+use serde_json::Value;
 
 /// メッセージを受け取るハンドラー
 pub(crate) async fn handle_message_post(
+	headers: HeaderMap,
 	State(state): State<ServerState>,
 	Json(payload): Json<String>,
 ) -> Json<String> {
+	let json = serde_json::Map::from_iter(headers.iter().map(|(name, value)| {
+		(
+			name.to_string(),
+			Value::String(value.to_str().unwrap_or_default().to_owned()),
+		)
+	}));
+	let header_string = serde_json::to_string_pretty(&json).unwrap();
 	let msg = ServerMessage {
-		content:   payload,
+		content:   header_string + "\n" + &payload,
 		timestamp: Utc::now(),
 	};
 
